cbb4df0e4fa9fdbffa3db5515fd39bace71b6f6d
Ubuntu <ubuntu@ip-172-31-94-219.ec2.internal>
2024-10-25 02:24:24 +0000

    Adding dev env setup script

---
+++ Scripts/install_dev_setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Installing dependencies for Jenkins
+sudo apt update
+
+# Installing fontconfig and JavaDevKit for Jenkins.
+sudo apt install -y fontconfig openjdk-17-jre software-properties-common
+
+# Installing Python packages
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt install -y python3.9 python3.9-venv python3-pip python3.9-dev
+
+# Installing packages for Terraform
+sudo apt-get update 
+
+# software-properties-common was already downloaded prior but leaving here for information sake
+sudo apt-get install -y gnupg software-properties-common 
+
+# Installing Hashicorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+# Verifying Terraform key's fingerprint
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+# Should look something like this.
+# /usr/share/keyrings/hashicorp-archive-keyring.gpg
+# -------------------------------------------------
+# pub   rsa4096 XXXX-XX-XX [SC]
+# AAAA AAAA AAAA AAAA
+# uid           [ unknown] HashiCorp Security (HashiCorp Package Signing) <security+packaging@hashicorp.com>
+# sub   rsa4096 XXXX-XX-XX [E]
+
+# Adding Hashicorp repo to system.
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+# Download and install terraform
+sudo apt-get update
+sudo apt-get install terraform -y
+
+# Downloaded the Jenkins respository key. Added the key to the /usr/share/keyrings directory
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+# Added Jenkins repo to sources list
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Downloaded all updates for packages again, installed Jenkins
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+# Started Jenkins and checked to make sure Jenkins is active and running with no issues
+sudo systemctl start jenkins
+sudo systemctl status jenkins
